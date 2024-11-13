916e97146fbd7981d070da2dbe92e75f89921ffc
Praktek76 <proganteng1508@gmail.com>
2024-11-13 10:32:54 +0700

    d

---
+++ Belajar-bootstrap/Belajar-algoritma/variable.js
@@ -0,0 +1,46 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+const message = 'Hello world' // Try edit me
+var y = 'fugi';
+var jam = '';
+let p ='';
+
+// Log to console
+console.log(message)
+console.log(y)
+document.getElementById('a').innerHTML = jam,p;
+
+// percabangan
+if (jam > 12){
+  p = 'PM';
+}else {
+  p = 'AM';
+}
+if (jam > 24){
+  p = ''
+}
+
+// perulangan
+var s = "";
+for (var i = 0; i < 5; i++){
+    for (var j = 0; j <= i; j++){
+        s += "I";
+    }
+    s += "\n";
+}
+var h = "";
+for (var q = 5; q > 0; q--){
+    for (var w = 0; w < q; w++){
+        h += "I";
+    }
+    h += "\n";
+}
+
+//  log
+console.log(jam);
+console.log(p)
+console.log("\n")
+console.log(s)
+console.log(h)
